5ec6d22e74a628c85f235dba6873faec8373f33d
suchjose84 <suchjose@live.com>
2022-09-18 11:42:51 +0800

    hayzz

---
+++ js/main.js
@@ -0,0 +1,15 @@
+let ol = document.createElement('ol');
+let li = document.createElement('li');
+let p = document.createElement('p');
+
+ol.appendChild(li);
+ol.appendChild(p);
+p.innerText(pandak);
+
+
+
+
+document.querySelector('.weeks').appendChild(ol);
+
+
+
