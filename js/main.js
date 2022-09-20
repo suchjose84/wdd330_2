a44f23dedce9c07b597dc3d37fe36df4f9b754b0
suchjose84 <suchjose@live.com>
2022-09-21 03:52:27 +0800

    main page finished

---
+++ js/main.js
@@ -0,0 +1,28 @@
+const weeks = [{
+    label: "Week1 Notes",
+    url: "week1/index.html"
+},
+{
+    label: "Week2 Notes",
+    url: "week2/index.html"
+}
+]
+
+for (let i = 0; i < weeks.length; i++) {
+
+let li = document.createElement('li');
+let a = document.createElement('a');
+let textNode = document.createTextNode(`Week ${i+1}`);
+a.setAttribute('href', weeks[i].url);
+a.appendChild(textNode);
+
+li.appendChild(a);
+
+
+
+document.querySelector('.weeks').appendChild(li);
+
+
+
+
+}
