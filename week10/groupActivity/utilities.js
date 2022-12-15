74e578c0785ab57af0bd933ac4ae0956c76515c1
suchjose84 <suchjose@live.com>
2022-12-15 17:22:05 +0800

    adfadf

---
+++ week10/groupActivity/utilities.js
@@ -0,0 +1,25 @@
+function getJSON(url){
+    return fetch(url)
+        .then(function(response) {
+            if (!response.ok) {
+                throw Error(response.statusText);
+            } else {
+                return response.json();
+            }
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
+        
+        
+}
+
+const getLocation = function(options) {
+    return new Promise(function(resolve, reject) {
+        navigator.geolocation.getCurrentPosition(resolve, reject, options);
+    });
+};
+export function getJSON(url) { ... };
+export const getLocation = function(options){ ... };
+
+
